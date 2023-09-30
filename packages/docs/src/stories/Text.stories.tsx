66a0a205bfad580e0e5ef839a8408195b59743c6
Mayron Alves <mayronalves@alu.ufc.br>
2023-09-30 19:18:28 -0300

    Componente Text.

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Text, TextProps } from "@ignite-ui/react";
+
+export default {
+  title: "Typography/Text",
+  component: Text,
+  args: {
+    children:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione deserunt assumenda repellendus, recusandae nesciunt, iusto magni dolores culpa incidunt sit doloribus asperiores voluptates exercitationem obcaecati quam vero nam odit atque.",
+  },
+} as Meta<TextProps>;
+
+export const Primary: StoryObj<TextProps> = {};
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: "Strong text",
+    as: "strong",
+  },
+};
