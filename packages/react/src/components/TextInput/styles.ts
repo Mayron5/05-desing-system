60180f81ee4c889ff83d9aa38e3c19a0f5b0b1af
Mayron Alves <mayronalves@alu.ufc.br>
2023-10-01 14:57:09 -0300

    Componente do TextInput.

---
+++ packages/react/src/components/TextInput/styles.ts
@@ -0,0 +1,44 @@
+import { styled } from "../../styles";
+
+export const TextInputContainer = styled("div", {
+  backgroundColor: '$gray900',
+  padding: '$3 $4',
+  borderRadius: '$sm',
+  boxSizing: 'border-box',
+  border: '2px solid $gray900',
+
+  display: 'flex',
+  alignItems: 'baseline',
+
+  '&:has(input:focus)': {
+    borderColor: '$ignite300',
+    transition: 'all 300ms'
+  },
+
+  '&:has(input:disabled)': {
+    opacity: 0.5,
+    cursor: 'not-allowed'
+  }
+
+});
+
+export const Prefix = styled("span", {
+  fontFamily: '$default',
+  fontSize: '$sm',
+  color: '$gray400',
+  fontWeight: '$regular'
+});
+
+export const Input = styled("input", {
+  all: 'unset',
+  fontFamily: '$default',
+  fontSize: '$sm',
+  color: '$white',
+  fontWeight: '$regular',
+  backgroundColor: 'transparent',
+  width: '100%',
+
+  '&:placeholder': {
+    color: '$gray400'
+  }
+});
